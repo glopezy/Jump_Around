b882afe741a32d19ced690c71b34d9fd9549270e
glopezy <glopezy1999@gmail.com>
2025-01-16 10:24:49 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class PlayerController: MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private float inputH;
+
+    [SerializeField] private float velocidadMovimiento;
+    [SerializeField] private float jumpPower;
+
+    private bool isJumping = false;
+
+    private float time;
+
+    private bool isFacingRight = true;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputH =  Input.GetAxisRaw("Horizontal");
+        FlipSprite();
+
+        if(Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb.linearVelocityY = jumpPower;
+            isJumping = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocityX = inputH * velocidadMovimiento;
+    }
+
+    private void FlipSprite()
+    {
+        if (isFacingRight && inputH < 0f || !isFacingRight && inputH > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+        
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isJumping = false;
+    }
+}
