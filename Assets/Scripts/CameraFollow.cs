84e99af00429f40d709d371f70dd6fb1ed4ccfc6
glopezy <glopezy1999@gmail.com>
2025-01-17 11:19:03 +0100

    CameraBounds

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,6 +6,7 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private Transform target;
     [SerializeField] private Vector3 offset = new Vector3(7.44f,1.28f,-10f);
     [SerializeField] [Range(1,10)] private float smoothFactor;
+    [SerializeField] private Vector3 minValue, maxValue;
 
     // Update is called once per frame
     private void FixedUpdate()
@@ -17,8 +18,15 @@ public class CameraFollow : MonoBehaviour
 
     private void Follow()
     {
+        
+
         Vector3 targetPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor * Time.fixedDeltaTime);
+
+        Vector3 boundPosition = new Vector3(Mathf.Clamp(targetPosition.x, minValue.x, maxValue.x), Mathf.Clamp(targetPosition.y, minValue.y, maxValue.y), targetPosition.z);
+
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, boundPosition, smoothFactor * Time.fixedDeltaTime);
         transform.position = smoothedPosition;
+
+        
     }
 }
